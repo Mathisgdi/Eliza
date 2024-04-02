32bcf3b6ed4d97379f3e7612f9dc04b03d21e2c8
Mathisgdi <mathis.ghidotti@epitech.digital>
2024-04-02 15:35:44 +0200

    feat : readme

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# Eliza
+
+Eliza is a Discord bot connect to Chagpt 
+
+# Installation
+
+To install the project
+```
+npm install
+```
+# Configuration
+Create a Discord Bot and connect it to a discord server
+
+Put your OpenAI API key and your discord bot token in the **.env.exemple** and delete the **.exemple**
+
+# Deployment
+
+To run this project 
+
+```bash
+  node index.js
+```
+
--- index.js
+++ index.js
@@ -12,26 +12,24 @@ const client = new Client({
   ],
 });
 
-
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+    apiKey: process.env.OPENAI_API_KEY 
 });
 
 client.on("messageCreate", async function (message) {
-    if (message.author.bot) return;
+  if (message.author.bot) return;
     
   try {
     const stream = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [
-        {"role": "system", "content": "You are a helpful assistant who responds succinctly with only three phrases"},
+        {"role": "system", "content": "You are a helpful assistant who responds succinctly, do not share personal information, and do not use offensive language."},
         {"role": "user", "content": message.content}
       ],
       stream: true,
     });
     let reponse_message = ''
     for await (const part of stream) {
-      // reponse_message += part.choices[0].delta['content'];
       if (part.choices[0].delta && part.choices[0].delta['content'] ) {
         reponse_message += part.choices[0].delta['content'];
       }
